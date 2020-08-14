0015c6f3467c5d88224b1fdb47c193173785f4fe
gamcoh <cohengamliel8@gmail.com>
2020-08-14 18:52:38 +0200

    trained a vgg16 model (dina2)

--- src/train_model.py
+++ src/train_model.py
@@ -8,29 +8,31 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
-train_generator, val_generator, test_generator = get_generators(target_size=(135, 180), batch_size=32)
+train_generator, val_generator, test_generator = get_generators(target_size=(165, 210), batch_size=32)
 
-vgg16 = VGG16(include_top=False, weights="imagenet", input_shape=(135, 180, 3))
+vgg16 = VGG16(include_top=False, weights="imagenet", input_shape=(165, 210, 3))
 
 for layer in vgg16.layers:
     layer.trainable = False
 
 x = GlobalAveragePooling2D()(vgg16.output)
-x = Dense(256, activation='relu')(x)
+x = Dense(128, activation='relu')(x)
+x = Dense(64, activation='relu')(x)
+x = Dense(128, activation='relu')(x)
 x = Dense(11, activation='softmax')(x)
 
 model = Model(inputs=vgg16.input, outputs=x)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 callbacks = [
-    ModelCheckpoint('vgg16_freeze_256_img_135.h5', save_best_only=True),
+    ModelCheckpoint('vgg16_freeze_x64x_img_165_b32.h5', save_best_only=True),
     EarlyStopping(patience=5)
 ]
 
 model.fit_generator(
     train_generator,
     steps_per_epoch=200,
-    epochs=20,
+    epochs=10,
     validation_data=val_generator,
     validation_steps=200,
     callbacks=callbacks
