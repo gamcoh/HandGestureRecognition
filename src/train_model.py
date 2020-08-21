64ae2adb9fcf0240c29823bc0e6a888523370c32
gamcoh <cohengamliel8@gmail.com>
2020-08-21 11:22:02 +0200

    Testing new hyperparameters on the model

--- src/train_model.py
+++ src/train_model.py
@@ -5,10 +5,10 @@ from Utils import Utils
 
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
-train_generator, val_generator, test_generator = Utils.get_generators(target_size=(125, 180), batch_size=18)
+train_generator, val_generator, test_generator = Utils.get_generators(target_size=(135, 180), batch_size=18)
 
 vgg16 = VGG16(include_top=False, weights="imagenet", input_shape=(135, 180, 3))
 
@@ -16,14 +16,19 @@ for layer in vgg16.layers:
     layer.trainable = False
 
 x = GlobalAveragePooling2D()(vgg16.output)
-x = Dense(256, activation='relu')(x)
+x = Dense(162, activation='relu')(x)
+x = Dropout(.3)(x)
+x = Dense(86, activation='relu')(x)
+x = Dropout(.3)(x)
+x = Dense(162, activation='relu')(x)
+x = Dropout(.3)(x)
 x = Dense(11, activation='softmax')(x)
 
 model = Model(inputs=vgg16.input, outputs=x)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 callbacks = [
-    ModelCheckpoint('vgg16_freeze_256_img_165_b32.h5', save_best_only=True),
+    ModelCheckpoint('vgg16_freeze_x86x_img_135_b18.h5', save_best_only=True),
     EarlyStopping(patience=5)
 ]
 
