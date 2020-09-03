89c7da284d9b1ca72cf683d7c81bf7646d2546c4
gamcoh <cohengamliel8@gmail.com>
2020-09-03 14:40:41 +0200

    Added a trained model (dina5)

--- src/train_model.py
+++ src/train_model.py
@@ -6,7 +6,7 @@ from Utils import Utils
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 
 train_generator, val_generator, test_generator = Utils.get_generators(target_size=(135, 180), batch_size=18)
 
@@ -17,18 +17,17 @@ for layer in vgg16.layers:
 
 x = GlobalAveragePooling2D()(vgg16.output)
 x = Dense(162, activation='relu')(x)
-x = Dropout(.3)(x)
-x = Dense(86, activation='relu')(x)
-x = Dropout(.3)(x)
+x = Dropout(.5)(x)
 x = Dense(162, activation='relu')(x)
-x = Dropout(.3)(x)
+x = Dropout(.5)(x)
 x = Dense(11, activation='softmax')(x)
 
 model = Model(inputs=vgg16.input, outputs=x)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 callbacks = [
-    ModelCheckpoint('vgg16_freeze_x86x_img_135_b18.h5', save_best_only=True),
+    #TensorBoard(log_dir='../logs', histogram_freq=0, write_images=True, update_freq='batch'),
+    ModelCheckpoint('vgg16_freeze_162x2_img_135_b18.h5', save_best_only=True),
     EarlyStopping(patience=5)
 ]
 
@@ -42,7 +41,7 @@ def check_gen(gen):
 
 model.fit(
     check_gen(train_generator),
-    epochs=10,
+    epochs=4,
     steps_per_epoch=87605//32,
     validation_steps=300,
     validation_data=check_gen(val_generator),
