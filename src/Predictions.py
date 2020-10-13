f5d5898860a0fca4f38afba6cef93715238b07b7
gamcoh <cohengamliel8@gmail.com>
2020-10-13 14:40:07 +0200

    tested another model

--- src/Predictions.py
+++ src/Predictions.py
@@ -3,37 +3,37 @@ import cv2
 import numpy as np
 
 class Prediction():
-	def __init__(self):
-		self.model = load_model('./vgg16_freeze_162x2_img_135_b18.h5')
-		self.labels = [
-			"action1",
-			"action2",
-			"action3",
-			"action4",
-			"action5",
-			"action6",
-			"actionAmplifier1",
-			"actionAmplifier2",
-			"actionAmplifier3",
-			"actionAmplifier4",
-			"other"
-		]
+    def __init__(self):
+        self.model = load_model('../models/vgg16_freeze_142x2_img_320_b86.h5')
+        self.labels = [
+            "action1",
+            "action2",
+            "action3",
+            "action4",
+            "action5",
+            "action6",
+            "actionAmplifier1",
+            "actionAmplifier2",
+            "actionAmplifier3",
+            "actionAmplifier4",
+            "other"
+        ]
 
 
-	def get_action(self, image):
-		"""Predict the action based on the imahe
-		
-		Arguments:
-			image {numpy.ndarray} -- an image
-		
-		Returns:
-			string -- the predicted action label
-		"""		
-		img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		img = cv2.resize(img, (180, 135))
-		x = np.array(img, dtype="float32")
-		x = x.reshape(1, 135, 180, 3)
-		pred = self.model.predict(x)
-		
-		return self.labels[np.argmax(pred)]
+    def get_action(self, image):
+        """Predict the action based on the imahe
+
+        Arguments:
+            image {numpy.ndarray} -- an image
+
+        Returns:
+            string -- the predicted action label
+        """
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (320, 240))
+        x = np.array(img, dtype="float32")
+        x = x.reshape(1, 240, 320, 3)
+        pred = self.model.predict(x)
+
+        return self.labels[np.argmax(pred)]
 
--- src/run_action.py
+++ src/run_action.py
@@ -11,33 +11,33 @@ model = Prediction() # init the trained model for inference
 #action = Actions() # init the action class in order to lunch actions based on predictions
 
 while True:
-	sleep(5) # search for predictions every 30 seconds
+    sleep(5) # search for predictions every 30 seconds
 
-	cam = cv2.VideoCapture(0)
-	preds = []
-	for frame in Utils.getFrames(cam, s=10):
-		preds.append(model.get_action(frame))
-	cam.release()
+    cam = cv2.VideoCapture(0)
+    preds = []
+    for frame in Utils.getFrames(cam, s=10):
+        preds.append(model.get_action(frame))
+    cam.release()
 
-	# find the 2 most common preds in all the preds made
-	# once we got them we want to see if there is an amplifier
-	# if yes we execute both actions: amplifier, actionX
-	# if not we only search for the one most action found and execute it
-	most_common_preds = Utils.top_k(preds, k=2)
-	amplifierFounded, actionsWanted = Utils.hasAmplifier(most_common_preds)
-	print(actionsWanted)
-	continue
+    # find the 2 most common preds in all the preds made
+    # once we got them we want to see if there is an amplifier
+    # if yes we execute both actions: amplifier, actionX
+    # if not we only search for the one most action found and execute it
+    most_common_preds = Utils.top_k(preds, k=2)
+    amplifierFounded, actionsWanted = Utils.hasAmplifier(most_common_preds)
+    print(actionsWanted)
+    continue
 
-	if amplifierFounded:
-		for acc in actionsWanted:
-			action.run(acc)
-	else:
-		action.run(Utils.top_k(preds, k=1)[0])
+    if amplifierFounded:
+        for acc in actionsWanted:
+            action.run(acc)
+    else:
+        action.run(Utils.top_k(preds, k=1)[0])
 
-	keypress = cv2.waitKey(1) & 0xFF
-	if keypress == ord("q"):
-		cv2.destroyAllWindows()
-		break
+    keypress = cv2.waitKey(1) & 0xFF
+    if keypress == ord("q"):
+        cv2.destroyAllWindows()
+        break
 
 # When everything's done, release the capture
 cv2.destroyAllWindows()
